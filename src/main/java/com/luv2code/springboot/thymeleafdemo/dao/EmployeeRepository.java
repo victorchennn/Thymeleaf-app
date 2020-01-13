f7e38606a0b1669af82e95482a6489d085fab47b
Chen <victorchennn@gmail.com>
2020-01-13 17:52:58 +0000

    first commit

---
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -0,0 +1,18 @@
+package com.luv2code.springboot.thymeleafdemo.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.luv2code.springboot.thymeleafdemo.entity.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+	// that's it ... no need to write any code LOL!
+	
+	// add a method to sort by last name
+	public List<Employee> findAllByOrderByLastNameAsc();
+
+	public List<Employee> findByFirstNameContainsOrLastNameContainsAllIgnoreCase(String theName, String theName2);
+	
+}
